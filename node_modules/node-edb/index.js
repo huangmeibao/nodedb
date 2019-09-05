2875625906587af6df908a64a7af3597f54d6dca
huangmeibao <meibao.huang@argrace.ai>
2019-09-05 15:02:29 +0800

    nql.js

---
+++ node_modules/node-edb/index.js
@@ -0,0 +1,24 @@
+let Func = require('./lib/func')
+let parsing = require('./lib/parsing') 
+
+let nodedb = function() {
+  this.url= ''
+  this.db= ''
+  this.execute = new Func()
+  this.init=(url,db) => {
+    this.url = url
+    this.db = db
+    this.execute.init(url,db)
+  },
+  this.checkSys=()=>{ //系统扩展
+  }
+}
+let nodeDb = new nodedb()
+module.exports = nodeDb
+
+
+
+
+
+
+
